f81f7b7b63870ac55e5ef7fe4dfafa71fae05efc
Xd_pro <62808970+Xd-pro@users.noreply.github.com>
2021-05-26 18:24:23 +0100

    Initial Commit

---
+++ back/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__, "", "../front/dist", template_folder="../front/dist")
+
+@app.errorhandler(404)
+def catch_all(path):
+    print("R")
+    return render_template("index.html")
+
+@app.route("/api")
+@app.route("/api/")
+def api():
+    return "API"
+
+if __name__ == '__main__':
+    app.run()
---
+++ front/src/pages/index.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { metatags } from '@roxi/routify'
+    metatags.title = 'My Routify app'
+    metatags.description = 'Description coming soon...'
+</script>
+
+<h1>Routify + Svelte + Flask!</h1>
